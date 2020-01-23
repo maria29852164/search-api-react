e95a669bc2a94b7e081f4e9b754aecb60551fc65
maria de los angeles <mariaramireeez2002@gmail.com>
2020-01-23 17:47:55 -0400

    uso de la api pixabay

--- src/App.js
+++ src/App.js
@@ -7,8 +7,17 @@ export default class App extends Component {
     search:''
   }
   getSearch=(term)=>{
-    this.setState({search:term})
-    console.log(term)
+    this.setState({search:term},()=>this.queryApi())
+   
+
+
+  }
+  queryApi=()=>{
+    const url=`https://pixabay.com/api/?key=15015090-6a1514d74b47f80bc038339d3&q=${this.state.search}`
+    //console.log(url)
+    fetch(url).then(res=>{
+      return res.json()
+    }).then(data=>console.log(data))
 
   }
   
